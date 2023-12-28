084b6b843d6f910782fda44b39642bfb468c3a64
Zhengtao Huang <z.huang-43@student.tudelft.nl>
2023-12-28 10:55:27 +0100

    increased stack size so that the board does not crash

--- assignment-b/src/main.cpp
+++ assignment-b/src/main.cpp
@@ -15,21 +15,27 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define STACK_SIZE 512
+#define STACK_SIZE 2048
 
 void task_update_peripherals(void *, void *, void *);
 
 void task_check_keyboard(void *, void *, void *);
 
-void task_make_and_write_audio(void *, void *, void *);
+void task_write_audio(void *, void *, void *);
+
+void task_make_audio(void *, void *, void *);
 
 K_THREAD_STACK_DEFINE(stack0, STACK_SIZE);
 K_THREAD_STACK_DEFINE(stack1, STACK_SIZE);
 K_THREAD_STACK_DEFINE(stack2, STACK_SIZE);
+K_THREAD_STACK_DEFINE(stack3, STACK_SIZE);
 
 struct k_thread my_thread_data_0;
 struct k_thread my_thread_data_1;
 struct k_thread my_thread_data_2;
+struct k_thread my_thread_data_3;
+
+//K_MUTEX_DEFINE(my_mutex);
 
 Synthesizer synth;
 
@@ -84,63 +90,85 @@ int main(void) {
                                        K_THREAD_STACK_SIZEOF(stack0),
                                        task_update_peripherals,
                                        NULL, NULL, NULL,
-                                       5, 0, K_NO_WAIT);
+                                       4, 0, K_NO_WAIT);
     k_tid_t my_tid_1 = k_thread_create(&my_thread_data_1, stack1,
                                        K_THREAD_STACK_SIZEOF(stack1),
                                        task_check_keyboard,
                                        NULL, NULL, NULL,
-                                       5, 0, K_NO_WAIT);
+                                       4, 0, K_NO_WAIT);
     k_tid_t my_tid_2 = k_thread_create(&my_thread_data_2, stack2,
                                        K_THREAD_STACK_SIZEOF(stack2),
-                                       task_make_and_write_audio,
+                                       task_make_audio,
                                        NULL, NULL, mem_block,
                                        5, 0, K_NO_WAIT);
+//    k_tid_t my_tid_3 = k_thread_create(&my_thread_data_3, stack3,
+//                                       K_THREAD_STACK_SIZEOF(stack3),
+//                                       task_write_audio,
+//                                       NULL, NULL, mem_block,
+//                                       6, 0, K_NO_WAIT);
 
     k_thread_suspend(k_current_get());
     return 0;
 }
 
 void task_update_peripherals(void * p1, void * p2, void * p3) {
-    int64_t time = k_uptime_get();
+    int64_t time;
     while (1) {
-        if (k_uptime_get() - time > BLOCK_GEN_PERIOD_MS - 1) {
-            // Check the peripherals input
-            set_led(&debug_led0);
-            peripherals_update();
-            reset_led(&debug_led0);
-            k_yield();
-        }
+        time = k_uptime_get();
+        // Check the peripherals input
+        set_led(&debug_led0);
+        peripherals_update();
+        reset_led(&debug_led0);
+
+        k_sleep(K_MSEC(BLOCK_GEN_PERIOD_MS - (k_uptime_get() - time)));
     }
 }
 
 void task_check_keyboard(void * p1, void * p2, void * p3) {
-    int64_t time = k_uptime_get();
+    int64_t time;
     while (1) {
-        if (k_uptime_get() - time > BLOCK_GEN_PERIOD_MS - 1) {
-            // Get user input from the keyboard
-            set_led(&debug_led1);
-            check_keyboard();
-            reset_led(&debug_led1);
-            k_yield();
-        }
+        time = k_uptime_get();
+        // Get user input from the keyboard
+        set_led(&debug_led1);
+        check_keyboard();
+        reset_led(&debug_led1);
+
+        k_sleep(K_MSEC(BLOCK_GEN_PERIOD_MS - (k_uptime_get() - time)));
     }
 }
 
-void task_make_and_write_audio(void * p1, void * p2, void *mem_block) {
-    int64_t time = k_uptime_get();
+void task_make_audio(void * p1, void * p2, void *mem_block) {
+    int64_t time;
     while (1) {
-        if (k_uptime_get() - time > BLOCK_GEN_PERIOD_MS - 1) {
-            // Make synth sound
-            set_led(&debug_led2);
-            synth.makesynth((uint8_t *) mem_block);
-            reset_led(&debug_led2);
-            //k_yield();
+        time = k_uptime_get();
+        // Make synth sound
 
-            // Write audio block
-            set_led(&debug_led3);
-            writeBlock(mem_block);
-            reset_led(&debug_led3);
-            k_yield();
-        }
+        set_led(&debug_led2);
+        synth.makesynth((uint8_t *) mem_block);
+        reset_led(&debug_led2);
+
+        set_led(&debug_led3);
+        writeBlock(mem_block);
+        reset_led(&debug_led3);
+
+        k_sleep(K_MSEC(BLOCK_GEN_PERIOD_MS - (k_uptime_get() - time)));
+    }
+}
+
+void task_write_audio(void * p1, void * p2, void *mem_block) {
+    int64_t time;
+    while (1) {
+        //k_mutex_lock(&my_mutex, K_FOREVER);
+        time = k_uptime_get();
+        // Write audio block
+
+        set_led(&debug_led3);
+
+        //k_mutex_lock(&my_mutex, K_FOREVER);
+        writeBlock(mem_block);
+        //k_mutex_lock(&my_mutex, K_FOREVER);
+
+        reset_led(&debug_led3);
+        k_sleep(K_MSEC(BLOCK_GEN_PERIOD_MS - (k_uptime_get() - time)));
     }
 }
